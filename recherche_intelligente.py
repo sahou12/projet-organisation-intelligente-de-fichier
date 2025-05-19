e9ad2f91dde26aea6183799f74c858d126eae2da
sahou12 <44003885@parisnanterre.fr>
2025-05-19 16:08:39 +0200

    à supp

--- recherche_intelligente.py
+++ recherche_intelligente.py
@@ -1,17 +1,2 @@
-# from recherche_intelligente import lancer_recherche
-# elif choix == "2":
-#     lancer_recherche()
-import os
+supprime contenu fichier
 
-def lancer_recherche(base):
-    mot_cle = input("Entrez un mot-clé à rechercher : ").lower()
-    for dossier, _, fichiers in os.walk(base):
-        for fichier in fichiers:
-            chemin = os.path.join(dossier, fichier)
-            try:
-                with open(chemin, "r", encoding="utf-8") as f:
-                    contenu = f.read().lower()
-                    if mot_cle in contenu:
-                        print(f"🔍 Mot trouvé dans : {chemin}")
-            except:
-                continue
